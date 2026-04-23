8e24bb4e524fb1504b553407b8dcfcc177f5c005
csstudiosrj <cscomrj02@gmail.com>
2026-04-22 21:43:20 -0300

    tentando resolver conexão do banco de dados

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON!;
+const supabaseUrl = process.env.https://ebakpqrespckucqwfdok.supabase.co;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
