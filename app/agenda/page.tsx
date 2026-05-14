a770b0c71e5b24a4ee8dba89040c98fa1fd4df4b
csstudiosrj <cscomrj02@gmail.com>
2026-05-14 00:51:48 +0000

    ajuste

--- app/agenda/page.tsx
+++ app/agenda/page.tsx
@@ -1,4 +1,4 @@
-```tsx
+tsx
 "use client";
 
 import React, { useState, useEffect, useMemo } from "react";
--- app/configuracoes/page.tsx
+++ app/configuracoes/page.tsx
@@ -1,4 +1,4 @@
-```tsx
+tsx
 "use client";
 
 import React, { useState, useEffect } from "react";
